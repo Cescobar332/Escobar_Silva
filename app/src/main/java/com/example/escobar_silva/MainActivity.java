7f466b6dce42ae1b0ff169d8afc7ad12df5b9510
Cescobar332 <cescobar332@unab.edu.co>
2023-03-15 15:46:12 -0500

    Initial commit

---
+++ app/src/main/java/com/example/escobar_silva/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.escobar_silva;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Producto> listaPrincipalProductos;
+    private RecyclerView rvListadoProductos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        cargarDatos();
+
+        rvListadoProductos = findViewById(R.id.rv_listado_productos);
+
+        AdaptadorPersonalizado miAdaptador = new AdaptadorPersonalizado(listaPrincipalProductos);
+
+        miAdaptador.setOnItemClickListener(new AdaptadorPersonalizado.OnItemClickListener() {
+            @Override
+            public void onItemClick(Producto miProducto, int posicion) {
+                Toast.makeText(MainActivity.this, "Hice click desde activity " + miProducto.getNombre(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemBtnEliminarClick(Producto miProducto, int posicion) {
+                listaPrincipalProductos.remove(posicion);
+                miAdaptador.setListadoInformacion(listaPrincipalProductos);
+            }
+        });
+
+        rvListadoProductos.setAdapter(miAdaptador);
+        rvListadoProductos.setLayoutManager(new LinearLayoutManager(this));
+    }
+    public void cargarDatos(){
+        Producto producto1 = new Producto();
+        producto1.setNombre("Computador HP");
+        producto1.setPrecio(8000000.0);
+        producto1.setUrl("https://www.alkosto.com/medias/196786419003-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyMTQyMjh8aW1hZ2UvanBlZ3xoMDUvaDFhLzEzNDY0ODYwMjk1MTk4LzE5Njc4NjQxOTAwM18wMDFfNzUwV3g3NTBIfDcxZjRjYTUxN2M1MzQ0NTkxMGY4ODUwMWQ3Y2RkZWNhODk2Mjg0YjNmYmEyMjllZTc0OWMxZjYxMDIwNDQ5Yjc");
+
+        Producto producto2 = new Producto("Teclado DELL", 250000.0, "https://snpi.dell.com/snp/images/products/large/es-co~580-AFKZ/580-AFKZ.jpg");
+        Producto producto3 = new Producto("Mouse", 50000.0, "https://jyrtechnology.com.co/wp-content/uploads/2020/10/MGJR-033-1.png");
+
+        listaPrincipalProductos = new ArrayList<>();
+
+        listaPrincipalProductos.add(producto1);
+        listaPrincipalProductos.add(producto2);
+        listaPrincipalProductos.add(producto3);
+    }
+}
---
+++ app/src/main/java/com/example/escobar_silva/Producto.java
@@ -0,0 +1,45 @@
+package com.example.escobar_silva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Producto {
+
+    public String Nombre;
+    public Double Precio;
+    public String Url;
+    public Producto(String nombre, Double precio, String url) {
+        this.Nombre = nombre;
+        this.Precio = precio;
+        this.Url = url;
+    }
+    public Producto(){
+
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(Double precio) {
+        Precio = precio;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+
+}
